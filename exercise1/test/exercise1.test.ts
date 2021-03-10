8f1c23f0383ba430e3c6b6c143996802fbd977e2
Outsmart Digital <outsmartdigital@Outsmarts-MacBook-Pro.local>
2021-03-10 18:45:41 -0300

    teste

--- exercise1/test/exercise1.test.ts
+++
@@ -1,9 +0,0 @@
-import { sumNumber } from "../exercise1";
-
-describe("testing exercise1", () => {
-  test("Should return sum = 10 ", () => {
-    const array = [2, 5];
-    const response = sumNumber(array);
-    expect(response).toBe(10);
-  });
-});
